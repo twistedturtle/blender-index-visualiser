f19f82d6082ba36c91793a24049f64f76b92de46
twistedturtle <hindredkin@gmail.com>
2023-06-14 09:31:49 +0100

    remove unnecessary modal operator

--- index_visualiser.py
+++ index_visualiser.py
@@ -2,7 +2,7 @@
 bl_info = {
 	'name': 'Index Visualiser (BMesh)',
 	'author': 'twistedturtle',
-	'version': (0, 1, 0),
+	'version': (0, 2, 0),
 	'blender': (3, 5, 1),
 	'location': 'View3D > Overlays',
 	'warning': '',
@@ -22,7 +22,7 @@ from gpu_extras.batch import batch_for_shader
 from bpy_extras.view3d_utils import location_3d_to_region_2d as loc3d2d
 
 
-def draw_callback_px(self, context):
+def draw_callback_px(context):
 	'''Draw the labels'''
 	if context.mode != "EDIT_MESH":
 		return
@@ -96,45 +96,6 @@ def draw_callback_px(self, context):
 		blf.draw(font_id, index)
 
 
-class ModalDrawOperator(bpy.types.Operator):
-	"""Draw index labels"""
-	bl_idname = "view3d.drawindexlabels"
-	bl_label = "Show Indices"
-
-	_handle = None
-
-	@classmethod
-	def poll(cls, context):
-		return context.active_object is not None
-
-	def modal(self, context, event):
-		if context.area:
-			context.area.tag_redraw()
-
-		if not context.scene.indexvis.show_indices:
-			bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
-
-			return {"CANCELLED"}
-
-		return {"PASS_THROUGH"}
-
-	def invoke(self, context, event):
-		try:
-			if context.scene.indexvis.show_indices:
-				self._handle = bpy.types.SpaceView3D.draw_handler_add(
-					draw_callback_px, (self, context), 'WINDOW', 'POST_PIXEL')
-
-				context.window_manager.modal_handler_add(self)
-
-				return {"RUNNING_MODAL"}
-			else:
-				bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
-
-				return {'RUNNING_MODAL'}
-		except:
-			return {"CANCELLED"}
-
-
 
 
 class ToggleIndices(bpy.types.Operator):
@@ -152,6 +113,41 @@ class ToggleIndices(bpy.types.Operator):
 		return {"FINISHED"}
 
 
+class ReloadScripts(bpy.types.Operator):
+	"""Reload scripts even when the modal operator is running."""
+
+	bl_idname = "indexvis.reloadscripts"
+	bl_label = "Reload Scripts"
+
+	def execute(self, context):
+		try:
+			bpy.types.SpaceView3D.draw_handler_remove(IndexVisSettings.dh, 'WINDOW')
+		except:
+			pass
+
+		bpy.ops.script.reload()
+
+		if not bpy.app.timers.is_registered(reloadHandler):
+			bpy.app.timers.register(reloadHandler, first_interval=0.001)
+
+		return {"FINISHED"}
+
+def reloadHandler():
+	if bpy.context.scene.indexvis.show_indices:
+		IndexVisSettings.dh = bpy.types.SpaceView3D.draw_handler_add(
+					draw_callback_px, (bpy.context,), 'WINDOW', 'POST_PIXEL')
+
+def toggleDrawHandler(self, context):
+	if context.scene.indexvis.show_indices:
+		IndexVisSettings.dh = bpy.types.SpaceView3D.draw_handler_add(
+					draw_callback_px, (context,), 'WINDOW', 'POST_PIXEL')
+	else:
+		try:
+			bpy.types.SpaceView3D.draw_handler_remove(IndexVisSettings.dh, 'WINDOW')
+		except:
+			pass
+
+
 
 def draw_overlay(self, context):
 	layout = self.layout
@@ -165,44 +161,13 @@ def draw_overlay(self, context):
 	layout.operator(ReloadScripts.bl_idname, text="Reload Scripts")
 
 
-class ReloadScripts(bpy.types.Operator):
-	"""Reload scripts even when the modal operator is running."""
-
-	bl_idname = "indexvis.reloadscripts"
-	bl_label = "Reload Scripts"
-
-	def execute(self, context):
-		bpy.context.scene.indexvis.old = context.scene.indexvis.show_indices
-		context.scene.indexvis.show_indices = False
-
-		if not bpy.app.timers.is_registered(reloadHandler):
-			bpy.app.timers.register(reloadHandler, first_interval=0.2)
-
-		return {"FINISHED"}
-
-
-def reloadHandler():
-	'''Reload scripts after the modal has finished'''
-	bpy.ops.script.reload()
-	if not bpy.app.timers.is_registered(restart):
-			bpy.app.timers.register(restart, first_interval=0.0001)
-
-
-# Need to do it his way to avoid issues (can't remember what now)
-def restart():
-	bpy.context.scene.indexvis.show_indices = bpy.context.scene.indexvis.old
-
-
-def startModal(self, context):
-	if context.scene.indexvis.show_indices:
-		bpy.ops.view3d.drawindexlabels("INVOKE_DEFAULT")
 
 
 class IndexVisSettings(bpy.types.PropertyGroup):
 	text_size: bpy.props.IntProperty(name="Text Size", default=13, min=8, max=20)
 
-	show_indices:  bpy.props.BoolProperty(name="Indices", default=False, update=startModal)
-	old:  bpy.props.BoolProperty(name="old", default=False)
+	show_indices:  bpy.props.BoolProperty(name="Indices", default=False, update=toggleDrawHandler)
+
 
 	bg_color: bpy.props.FloatVectorProperty(
              name = "BG Color Picker",
@@ -216,6 +181,9 @@ class IndexVisSettings(bpy.types.PropertyGroup):
              default = (0.0, 1.0, 0.0, 1.0),
              size = 4
              )
+
+	draw_handler: bpy.props.PointerProperty(type=bpy.types.Object)
+
 
 
 class Keymaps:
@@ -240,11 +208,14 @@ class Keymaps:
 		self.keymaps.clear()
 
 
+
 keymaps = Keymaps(space_type="VIEW_3D")
 
 def register():
+	IndexVisSettings.dh = None
+	IndexVisSettings.show = False
+
 	bpy.types.VIEW3D_PT_overlay.append(draw_overlay)
-	bpy.utils.register_class(ModalDrawOperator)
 	bpy.utils.register_class(IndexVisSettings)
 	bpy.utils.register_class(ReloadScripts)
 	bpy.utils.register_class(ToggleIndices)
@@ -255,14 +226,20 @@ def register():
 	keymaps.add(ReloadScripts, "F5", "PRESS")
 
 
+
 def unregister():
 	keymaps.removeAll()
+	
+	try:
+		bpy.types.SpaceView3D.draw_handler_remove(IndexVisSettings.dh, 'WINDOW')
+	except:
+		pass
+
 	del bpy.types.Scene.indexvis
 
 	bpy.utils.unregister_class(ToggleIndices)
 	bpy.utils.unregister_class(ReloadScripts)
 	bpy.utils.unregister_class(IndexVisSettings)
-	bpy.utils.unregister_class(ModalDrawOperator)
 	bpy.types.VIEW3D_PT_overlay.remove(draw_overlay)
 
 
